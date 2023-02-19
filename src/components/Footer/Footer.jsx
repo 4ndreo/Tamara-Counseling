82e5f688036348a5c9efcdbcc80f2423aae4811e
4ndreo <franjandreo@gmail.com>
2023-02-19 16:52:53 -0300

    animaciones y ajustes en colores terminadas

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,32 @@
+import "./Footer.css";
+
 import React from "react";
 import Container from "react-bootstrap/Container";
 
 export default function Footer() {
   return (
-    <Container id="contacto">
-      <h2>Contacto</h2>
-      <ul>
-        <li>Teléfono</li>
-        <li>Mail</li>
-        <li>Instagram</li>
-      </ul>
-    </Container>
+    <footer id="contacto">
+      <Container>
+        <h2>Contacto</h2>
+        <ul className="mt-4 mb-4">
+          <li>
+            <p className="negritas mb-2">Teléfono</p>
+            <a href="https://wa.me/+541112345678" target="blank">
+              +54 11 1234-5678
+            </a>
+          </li>
+          <li>
+            <p className="negritas mb-2">Mail</p>
+            <a href="mailto:yotucounseling@gmail.com">
+              yotucounseling@gmail.com
+            </a>
+          </li>
+          <li>
+            <p className="negritas mb-2">Instagram</p>
+            <span>@yotucounseling</span>
+          </li>
+        </ul>
+      </Container>
+    </footer>
   );
 }
