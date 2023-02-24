7608974c8cc6e0f412c40243f4956c271f1b708a
dreo <79935220+4ndreo@users.noreply.github.com>
2023-02-23 21:37:06 -0300

    new carousel

--- src/components/CarrouselQuotes/CarrouselQuotes.jsx
+++ src/components/CarrouselQuotes/CarrouselQuotes.jsx
@@ -1,11 +1,76 @@
 import "./CarrouselQuotes.css";
+import ReactSwipe from "react-swipe";
 
 import React from "react";
 
 export default function CarrouselQuotes() {
+  let reactSwipeEl;
+
+  // const mySwipe = new Swipe(document.getElementById('slider'), {
+  //   startSlide: 2,
+  //   speed: 400,
+  //   widthOfSiblingSlidePreview: 10,
+  //   auto: 3000,
+  //   continuous: true,
+  //   disableScroll: false,
+  //   stopPropagation: false,
+  //   callback: function(index, elem) {},
+  //   transitionEnd: function(index, elem) {}
+  // });
+
   return (
     <section className="c-quotes-cont">
-      <swiper-container
+      <div className="swipe-cont">
+      <ReactSwipe
+        className="carousel"
+        swipeOptions={{ continuous: true, auto: 3000, speed: 700, }}
+        
+        ref={(el) => (reactSwipeEl = el)}
+      >
+        <div class="slide-cont slide-1">
+          <div className="slide-text-cont">
+            <div className="container slide-text">
+              <h3>Slide 1</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+                nesciunt dolores, dolore porro quibusdam totam doloribus. Ipsa
+                dolores dolor dignissimos enim veniam voluptatum facilis ipsum!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="slide-cont slide-2">
+          <div className="slide-text-cont">
+            <div className="container slide-text">
+              <h3>Slide 2</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus, earum. Provident eligendi tempore voluptate
+                repellendus, rerum consequuntur dicta sed sequi.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="slide-cont slide-3">
+          <div className="slide-text-cont">
+            <div className="container slide-text">
+              <h3>Slide 3</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reiciendis quasi rerum ab aut assumenda, dolor omnis. Culpa
+                reiciendis, sapiente quaerat possimus iste ut! Impedit similique
+                optio blanditiis deleniti. Corrupti, voluptate.
+              </p>
+            </div>
+          </div>
+        </div>
+      </ReactSwipe>
+      <i onClick={() => reactSwipeEl.prev()} className="action-btn btn-prev fa-solid fa-circle-arrow-left"></i>
+      <i onClick={() => reactSwipeEl.next()} className="action-btn btn-next fa-solid fa-circle-arrow-right"></i>
+
+      </div>
+
+      {/* <swiper-container
         effect={"fade"}
         spaceBetween={30}
         centeredSlides={true}
@@ -18,6 +83,7 @@ export default function CarrouselQuotes() {
         navigation={true}
         loop="true"
       >
+        <div class="swiper-button-prev"></div>
         <swiper-slide class="slide-cont slide-1">
           <div className="slide-text-cont">
             <div className="container slide-text">
@@ -55,7 +121,7 @@ export default function CarrouselQuotes() {
             </div>
           </div>
         </swiper-slide>
-      </swiper-container>
+      </swiper-container> */}
     </section>
   );
 }
