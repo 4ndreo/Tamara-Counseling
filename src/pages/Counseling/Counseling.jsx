b49a3c2924cf15012a008681e4420610b232bc6e
dreo <79935220+4ndreo@users.noreply.github.com>
2023-02-25 02:36:19 -0300

    adding woman img on counseling

--- src/pages/Counseling/Counseling.jsx
+++ src/pages/Counseling/Counseling.jsx
@@ -1,4 +1,5 @@
 import "./Counseling.css";
+import WomanCounseling from "../../img/woman-counseling.jpg";
 
 import React from "react";
 
@@ -6,63 +7,82 @@ export default function Counseling() {
   return (
     <main className="counseling-cont container">
       <h1 className="pt-4 mb-4">Counseling: volviendo a tu balance </h1>
-      <p>
-        El Counseling es una profesión para el desarrollo y el despliegue de las
-        capacidades y las potencialidades del ser humano y para su bienestar.
-      </p>
-      <p>
-        Cada persona tiene su individualidad y la expresa de diferentes modos en
-        la vida, através de sus palabras, de sus sentimientos, de sus emociones,
-        de sus conductas.
-      </p>
-      <p>
-        El counseling crea condiciones que faciltan la expresion de la persona.
-        Trabajando con el vinculo humano, en un espacio de consulta. Facilitando
-        la toma de contacto con el presente, con la vivencia del momento.
-      </p>
-      <p>
-        La finalidad es el bienestar de la persona y que en el avance del
-        proceso pueda: reconocerse así misma con sus necesidades y hacer frente
-        a ellas, poniendo al servicio sus recursos internos y sus herramientas
-        personales; desarrollar y desplegar sus capacidades; ampliar las
-        posibilidades de elección; tomar mejores desiciones; contactar con sus
-        emociones, sentimientos, pensamientos y ser su propia guía; resolver con
-        mayor solvencia ciertas dificultades u obstáculos; empoderarse de si
-        mísma; sentirse funcionando plenamente; mejorar su calidad de vida, sus
-        vínculos, sus relaciones con el mundo; desenvolverse creativamente;
-        habilitarse a transitar la vida desplegando todo su ser, deseando,
-        sintiendo, pensando, haciendo, creando.
-      </p>
-      <p>
-        Como counselor puedo ofrecer una invitación a habitar un espacio de
-        escucha de lo que esté atravesando la persona en el momento presente.
-        Para que se permita expresar en un ambiente de seguridad, de respeto, de
-        comprensión.
-      </p>
-      <p>
-        Una de las ventajas importantes del trabajo en tiempo presente, es la
-        posibilidad de Resignificar situaciones pasadas, es decir de percibir
-        “que me sucede hoy con eso que pasó”, y darle un nuevo sentido, y
-        avanzar más livianos por la vida.
-      </p>
-      <p>
-        Por el hecho de vivir, nos vemos atravesados y afectados por diferentes
-        situaciones, y a veces necesitamos ser acompañados para habilitar lo que
-        nos pasa, ser escuchados, comprendidos como seres exclusivos.
-      </p>
-      <p>
-        Desde el counseling realizamos un acompañamiento de la persona, como una
-        realidad compleja y única, intentando acercarnos a su vivencia
-        particular. Y desde este lugar trabajamos con ella, en las necesidades
-        que fueran surgiendo durante el proceso.
-      </p>
-      <p>
-        Promoviendo el autoconocimiento y el desarrollo de la persona.
-        Acompañando el sufrimiento como ser en el mundo, con miras al despliegue
-        del potencial humano.
-      </p>
-      <h2>Temas que aborda el counseling</h2>
-      <swiper-container class="mySwiper" effect="cards" grab-cursor="true" autoplay="true" autoplay-delay="1500" autoplay-disable-on-interaction="false">
+      <div className="counseling-info-cont">
+        <div className="counseling-text-cont">
+          <p>
+            El Counseling es una profesión para el desarrollo y el despliegue de
+            las capacidades y las potencialidades del ser humano y para su
+            bienestar.
+          </p>
+          <p>
+            Cada persona tiene su individualidad y la expresa de diferentes
+            modos en la vida, através de sus palabras, de sus sentimientos, de
+            sus emociones, de sus conductas.
+          </p>
+          <p>
+            El counseling crea condiciones que faciltan la expresion de la
+            persona. Trabajando con el vinculo humano, en un espacio de
+            consulta. Facilitando la toma de contacto con el presente, con la
+            vivencia del momento.
+          </p>
+          <p>
+            La finalidad es el bienestar de la persona y que en el avance del
+            proceso pueda: reconocerse así misma con sus necesidades y hacer
+            frente a ellas, poniendo al servicio sus recursos internos y sus
+            herramientas personales; desarrollar y desplegar sus capacidades;
+            ampliar las posibilidades de elección; tomar mejores desiciones;
+            contactar con sus emociones, sentimientos, pensamientos y ser su
+            propia guía; resolver con mayor solvencia ciertas dificultades u
+            obstáculos; empoderarse de si mísma; sentirse funcionando
+            plenamente; mejorar su calidad de vida, sus vínculos, sus relaciones
+            con el mundo; desenvolverse creativamente; habilitarse a transitar
+            la vida desplegando todo su ser, deseando, sintiendo, pensando,
+            haciendo, creando.
+          </p>
+          <p>
+            Como counselor puedo ofrecer una invitación a habitar un espacio de
+            escucha de lo que esté atravesando la persona en el momento
+            presente. Para que se permita expresar en un ambiente de seguridad,
+            de respeto, de comprensión.
+          </p>
+          <p>
+            Una de las ventajas importantes del trabajo en tiempo presente, es
+            la posibilidad de Resignificar situaciones pasadas, es decir de
+            percibir “que me sucede hoy con eso que pasó”, y darle un nuevo
+            sentido, y avanzar más livianos por la vida.
+          </p>
+          <p>
+            Por el hecho de vivir, nos vemos atravesados y afectados por
+            diferentes situaciones, y a veces necesitamos ser acompañados para
+            habilitar lo que nos pasa, ser escuchados, comprendidos como seres
+            exclusivos.
+          </p>
+          <p>
+            Desde el counseling realizamos un acompañamiento de la persona, como
+            una realidad compleja y única, intentando acercarnos a su vivencia
+            particular. Y desde este lugar trabajamos con ella, en las
+            necesidades que fueran surgiendo durante el proceso.
+          </p>
+          <p>
+            Promoviendo el autoconocimiento y el desarrollo de la persona.
+            Acompañando el sufrimiento como ser en el mundo, con miras al
+            despliegue del potencial humano.
+          </p>
+        </div>
+        <div className="counseling-img-cont">
+
+        <img src={WomanCounseling} alt="" />
+        </div>
+      </div>
+      <h2 className="text-center">Temas que aborda el counseling</h2>
+      <swiper-container
+        class="mySwiper"
+        effect="cards"
+        grab-cursor="true"
+        autoplay="true"
+        autoplay-delay="1500"
+        autoplay-disable-on-interaction="false"
+      >
         <swiper-slide>
           <span>Etapas vitales</span>
         </swiper-slide>
