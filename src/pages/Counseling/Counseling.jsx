a1ae38f9696006c36f25489258fa269bfd2ec318
dreo <79935220+4ndreo@users.noreply.github.com>
2023-02-25 01:28:16 -0300

    adding content to counseling

--- src/pages/Counseling/Counseling.jsx
+++ src/pages/Counseling/Counseling.jsx
@@ -7,36 +7,116 @@ export default function Counseling() {
     <main className="counseling-cont container">
       <h1 className="pt-4 mb-4">Counseling: volviendo a tu balance </h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores
-        recusandae deserunt voluptatibus quasi nemo, dolorem, totam magni neque
-        blanditiis ullam unde. Enim dolorum ipsa accusantium cupiditate aut
-        exercitationem illum quos nihil, quibusdam asperiores iusto velit
-        necessitatibus aliquid, nemo ut! Provident saepe magni autem corporis
-        accusamus repellat velit neque, exercitationem nobis.
+        El Counseling es una profesión para el desarrollo y el despliegue de las
+        capacidades y las potencialidades del ser humano y para su bienestar.
       </p>
       <p>
-        At ab est voluptate iure rem, explicabo corrupti delectus sapiente sequi
-        quidem quia voluptatum vel dicta aliquam! Velit vitae explicabo, quo
-        eligendi facilis dolor officiis vel fugiat laudantium reiciendis rerum
-        aliquid iste tenetur molestiae exercitationem. Nostrum sapiente
-        similique, temporibus velit, nobis earum ducimus iste eaque numquam
-        ipsam pariatur aspernatur voluptatem?
+        Cada persona tiene su individualidad y la expresa de diferentes modos en
+        la vida, através de sus palabras, de sus sentimientos, de sus emociones,
+        de sus conductas.
       </p>
       <p>
-        Numquam repellat similique dolorum cumque quo tempora harum
-        reprehenderit, fuga accusantium consequuntur velit at doloremque totam
-        molestiae autem neque magnam rem. Et explicabo autem deleniti laudantium
-        architecto perspiciatis natus, neque, in adipisci, magnam sunt repellat
-        inventore aut! Veniam, exercitationem sequi eius atque magnam delectus,
-        expedita aspernatur tempora impedit natus et.
+        El counseling crea condiciones que faciltan la expresion de la persona.
+        Trabajando con el vinculo humano, en un espacio de consulta. Facilitando
+        la toma de contacto con el presente, con la vivencia del momento.
       </p>
       <p>
-        Consequuntur, sunt ab, earum rerum vero hic ea quos magnam, eum eligendi
-        odit! Blanditiis alias suscipit laboriosam optio, et, facere earum
-        explicabo nam placeat sint laudantium fuga nesciunt consequatur, quas
-        architecto culpa voluptas non voluptates doloribus consequuntur. Natus,
-        nisi rerum quis veniam maiores, dolor, voluptate aliquid ex excepturi
-        fugiat tempore!
+        La finalidad es el bienestar de la persona y que en el avance del
+        proceso pueda: reconocerse así misma con sus necesidades y hacer frente
+        a ellas, poniendo al servicio sus recursos internos y sus herramientas
+        personales; desarrollar y desplegar sus capacidades; ampliar las
+        posibilidades de elección; tomar mejores desiciones; contactar con sus
+        emociones, sentimientos, pensamientos y ser su propia guía; resolver con
+        mayor solvencia ciertas dificultades u obstáculos; empoderarse de si
+        mísma; sentirse funcionando plenamente; mejorar su calidad de vida, sus
+        vínculos, sus relaciones con el mundo; desenvolverse creativamente;
+        habilitarse a transitar la vida desplegando todo su ser, deseando,
+        sintiendo, pensando, haciendo, creando.
+      </p>
+      <p>
+        Como counselor puedo ofrecer una invitación a habitar un espacio de
+        escucha de lo que esté atravesando la persona en el momento presente.
+        Para que se permita expresar en un ambiente de seguridad, de respeto, de
+        comprensión.
+      </p>
+      <p>
+        Una de las ventajas importantes del trabajo en tiempo presente, es la
+        posibilidad de Resignificar situaciones pasadas, es decir de percibir
+        “que me sucede hoy con eso que pasó”, y darle un nuevo sentido, y
+        avanzar más livianos por la vida.
+      </p>
+      <p>
+        Por el hecho de vivir, nos vemos atravesados y afectados por diferentes
+        situaciones, y a veces necesitamos ser acompañados para habilitar lo que
+        nos pasa, ser escuchados, comprendidos como seres exclusivos.
+      </p>
+      <p>
+        Desde el counseling realizamos un acompañamiento de la persona, como una
+        realidad compleja y única, intentando acercarnos a su vivencia
+        particular. Y desde este lugar trabajamos con ella, en las necesidades
+        que fueran surgiendo durante el proceso.
+      </p>
+      <p>
+        Promoviendo el autoconocimiento y el desarrollo de la persona.
+        Acompañando el sufrimiento como ser en el mundo, con miras al despliegue
+        del potencial humano.
+      </p>
+      <h2>Temas que aborda el counseling</h2>
+      <swiper-container class="mySwiper" effect="cards" grab-cursor="true">
+        <swiper-slide>
+          <span>Etapas vitales</span>
+        </swiper-slide>
+        <swiper-slide>
+          <span>Identidad</span>
+        </swiper-slide>
+        <swiper-slide>
+          <span>Adolescencia</span>
+        </swiper-slide>
+        <swiper-slide>
+          <span>Pareja y familia</span>
+        </swiper-slide>
+        <swiper-slide>
+          <span>Educación</span>
+        </swiper-slide>
+        <swiper-slide>
+          <span>Adicciones</span>
+        </swiper-slide>
+        <swiper-slide>
+          <span>Desarrollo personal</span>
+        </swiper-slide>
+        <swiper-slide>
+          <span>Sexualidad</span>
+        </swiper-slide>
+        <swiper-slide>
+          <span>Espiritualidad</span>
+        </swiper-slide>
+        <swiper-slide>
+          <span>Laboral</span>
+        </swiper-slide>
+      </swiper-container>
+
+      <p>Los pilares fundamentales que sostienen nuestra profesión son:</p>
+      <ul>
+        <li>
+          - La formación académica teórico-vivencial, en la que abordamos el
+          potencial humano con sus aspectos biológicos, psíquicos, sociológicos
+          y espirituales. Con la responsabilidad de una formación continua en
+          todo lo que contribuya a nuestro despliegue personal de modo creativo,
+          a enriquecer nuestra existencia como personas en el mundo, y a la
+          facilitación de la escucha como counselors.
+        </li>
+        <li>- El proceso personal.</li>
+        <li>
+          - La supervisión del trabajo. En estos pilares nos apoyamos para
+          desarrollar las actitudes personales y profesionales necesarias para
+          crear condiciones facilitadoras de un vínculo humano y una
+          comunicación fluida en cada encuentro.
+        </li>
+      </ul>
+      <p>
+        En estos pilares nos apoyamos para desarrollar las actitudes personales
+        y profesionales necesarias para crear condiciones facilitadoras de un
+        vínculo humano y una comunicación fluida en cada encuentro.
       </p>
     </main>
   );
