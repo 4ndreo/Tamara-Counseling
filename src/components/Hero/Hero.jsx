bcb9da93f0b2a2e8a276f93446cfef4a22ffb662
dreo <79935220+4ndreo@users.noreply.github.com>
2023-03-27 11:35:21 -0300

    ajustes hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,10 +6,9 @@ export default function Hero() {
   return (
     <section className="hero-cont bg-gray">
       <div className="container">
-        <h1 className="hero-card-title mb-5">YO-TU Counseling</h1>
-        <p>Consultoría psicológica para el desarrollo personal</p>
-        <p>Acompañamiento y orientación psicológica</p>
-        <p>Psicología Humanista - Enfoque Centrado en la Persona</p>
+        <h1 className="hero-card-title mb-5"><span className="hero-yotu">YO-TU</span> <span className="hero-counseling">Counseling</span></h1>
+        <p className="hero-sub-1">Consultoría psicológica para el desarrollo personal</p>
+        <p className="hero-sub-2">Psicología Humanista - Enfoque Centrado en la Persona</p>
       </div>
     </section>
   );
