0f5836d026af0b884486a4763afd32bbba9ba082
dreo <79935220+4ndreo@users.noreply.github.com>
2023-03-30 22:47:35 -0300

    ajustes tamara

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,8 +7,10 @@ export default function Hero() {
     <section className="hero-cont bg-gray">
       <div className="container">
         <h1 className="hero-card-title mb-5"><span className="hero-yotu">YO-TU</span> <span className="hero-counseling">Counseling</span></h1>
+        <div className="hero-bajada">
         <p className="hero-sub-1">Consultoría psicológica para el desarrollo personal</p>
         <p className="hero-sub-2">Psicología Humanista - Enfoque Centrado en la Persona</p>
+        </div>
       </div>
     </section>
   );
