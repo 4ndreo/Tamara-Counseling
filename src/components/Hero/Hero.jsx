fe9867fbbcfbb2589946b5dfff8eb6d806569bc1
dreo <79935220+4ndreo@users.noreply.github.com>
2023-04-02 19:39:58 -0300

    ajuste tamaños en encabezado

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,11 +6,11 @@ export default function Hero() {
   return (
     <section className="hero-cont bg-gray">
       <div className="container">
-        {/* <h1 className="hero-card-title mb-5"><span className="hero-yotu">YO-TU</span> <span className="hero-counseling">Counseling</span></h1>
+        <h1 className="hero-card-title mb-5"><span className="hero-yotu">YO-TU</span> <span className="hero-counseling">Counseling</span></h1>
         <div className="hero-bajada">
         <p className="hero-sub-1">Consultoría psicológica para el desarrollo personal</p>
-        <p className="hero-sub-2">Psicología Humanista - Enfoque Centrado en la Persona</p> */}
-        {/* </div> */}
+        <p className="hero-sub-2">Psicología Humanista - Enfoque Centrado en la Persona</p>
+        </div>
       </div>
     </section>
   );
