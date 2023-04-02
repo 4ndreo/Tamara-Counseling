4d77ceadf4296858ac539e8c5dfc12a2f8abe867
dreo <79935220+4ndreo@users.noreply.github.com>
2023-04-02 20:19:57 -0300

    encabezado responsive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
         <h1 className="hero-card-title mb-5"><span className="hero-yotu">YO-TU</span> <span className="hero-counseling">Counseling</span></h1>
         <div className="hero-bajada">
         <p className="hero-sub-1">Consultoría psicológica para el desarrollo personal</p>
-        <p className="hero-sub-2">Psicología Humanista<div className="hero-guion"> - </div><div className="hero-enfoque">Enfoque Centrado en la Persona</div></p>
+        <p className="hero-sub-2">Psicología Humanista<span className="hero-guion"> - </span><span className="hero-enfoque">Enfoque Centrado en la Persona</span></p>
         </div>
       </div>
     </section>
