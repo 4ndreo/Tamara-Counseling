eb65b585a3e7ee81092305b22c7029c3f0cb2247
dreo <79935220+4ndreo@users.noreply.github.com>
2023-04-03 14:05:38 -0300

    ajustes tamara

--- src/pages/Counseling/Counseling.jsx
+++ src/pages/Counseling/Counseling.jsx
@@ -9,9 +9,9 @@ export default function Counseling() {
       <div className="counseling-info-cont">
         <div className="counseling-text-cont">
           <p>
-            El <span className="negritas">Counseling</span> es una profesión para el desarrollo y el despliegue de
-            las capacidades y las potencialidades del ser humano y para su
-            bienestar.
+            El <span className="negritas">Counseling</span> es una profesión
+            para el desarrollo y el despliegue de las capacidades y las
+            potencialidades del ser humano y para su bienestar.
           </p>
           <p>
             Cada persona tiene su individualidad y la expresa de diferentes
@@ -19,36 +19,56 @@ export default function Counseling() {
             sus emociones, de sus conductas.
           </p>
           <p>
-            El counseling <span className="negritas">crea condiciones que faciltan la expresion de la
-            persona</span>. Trabajando con el vinculo humano, en un espacio de
-            consulta. Facilitando la toma de contacto con el presente, con la
-            vivencia del momento.
+            El counseling{" "}
+            <span className="negritas">
+              crea condiciones que faciltan la expresion de la persona
+            </span>
+            . Trabajando con el vinculo humano, en un espacio de consulta.
+            Facilitando la toma de contacto con el presente, con la vivencia del
+            momento.
           </p>
           <p>
-          <span className="negritas">La finalidad es el bienestar de la persona</span> y que en el avance del
-            proceso pueda: <span className="negritas">reconocerse a sí misma con sus necesidades</span> y hacer
-            frente a ellas, poniendo al servicio sus recursos internos y sus
-            herramientas personales; <span className="negritas">desarrollar y desplegar sus capacidades;
-            ampliar las posibilidades de elección; tomar mejores desiciones;
-            contactar con sus emociones, sentimientos, pensamientos y ser su
-            propia guía; resolver con mayor solvencia ciertas dificultades u
-            obstáculos; empoderarse de si mísma; sentirse funcionando
-            plenamente; mejorar su calidad de vida</span>, sus vínculos, sus relaciones
-            con el mundo; <span className="negritas">desenvolverse creativamente; habilitarse a transitar
-            la vida desplegando todo su ser</span>, deseando, sintiendo, pensando,
-            haciendo, creando.
+            <span className="negritas">
+              La finalidad es el bienestar de la persona
+            </span>{" "}
+            y que en el avance del proceso pueda:{" "}
+            <span className="negritas">
+              reconocerse a sí misma con sus necesidades
+            </span>{" "}
+            y hacer frente a ellas, poniendo al servicio sus recursos internos y
+            sus herramientas personales;{" "}
+            <span className="negritas">
+              desarrollar y desplegar sus capacidades; ampliar las posibilidades
+              de elección; tomar mejores desiciones; contactar con sus
+              emociones, sentimientos, pensamientos y ser su propia guía;
+              resolver con mayor solvencia ciertas dificultades u obstáculos;
+              empoderarse de si mísma; sentirse funcionando plenamente; mejorar
+              su calidad de vida
+            </span>
+            , sus vínculos, sus relaciones con el mundo;{" "}
+            <span className="negritas">
+              desenvolverse creativamente; habilitarse a transitar la vida
+              desplegando todo su ser
+            </span>
+            , deseando, sintiendo, pensando, haciendo, creando.
           </p>
           <p>
-            Como counselor <span className="negritas">puedo ofrecer una invitación a habitar un espacio de
-            escucha de lo que esté atravesando la persona en el momento
-            presente</span>. Para que se permita expresar en un ambiente de seguridad,
-            de respeto, de comprensión.
+            Como counselor{" "}
+            <span className="negritas">
+              puedo ofrecer una invitación a habitar un espacio de escucha de lo
+              que esté atravesando la persona en el momento presente
+            </span>
+            . Para que se permita expresar en un ambiente de seguridad, de
+            respeto, de comprensión.
           </p>
           <p>
             Una de las ventajas importantes del trabajo en tiempo presente, es
-            la <span className="negritas">posibilidad de Resignificar situaciones pasadas</span>, es decir de
-            percibir “que me sucede hoy con eso que pasó”, y darle un nuevo
-            sentido, y avanzar más livianos por la vida.
+            la{" "}
+            <span className="negritas">
+              posibilidad de Resignificar situaciones pasadas
+            </span>
+            , es decir de percibir “que me sucede hoy con eso que pasó”, y darle
+            un nuevo sentido, y avanzar más livianos por la vida.
           </p>
           <p>
             Por el hecho de vivir, nos vemos atravesados y afectados por
@@ -57,10 +77,13 @@ export default function Counseling() {
             exclusivos.
           </p>
           <p>
-            Desde el counseling <span className="negritas">realizamos un acompañamiento de la persona, como
-            una realidad compleja y única, intentando acercarnos a su vivencia
-            particular</span>. Y desde este lugar trabajamos con ella, en las
-            necesidades que fueran surgiendo durante el proceso.
+            Desde el counseling{" "}
+            <span className="negritas">
+              realizamos un acompañamiento de la persona, como una realidad
+              compleja y única, intentando acercarnos a su vivencia particular
+            </span>
+            . Y desde este lugar trabajamos con ella, en las necesidades que
+            fueran surgiendo durante el proceso.
           </p>
           <p>
             Promoviendo el autoconocimiento y el desarrollo de la persona.
@@ -75,46 +98,68 @@ export default function Counseling() {
       <section className="temas-counseling-cont">
         <h2 className="text-center">Temas que aborda el counseling</h2>
         <ul className="temas-counseling-list d-flex">
-          <li className="pe-1">Etapas vitales<span className="counseling-guion"> - </span></li>
-          <li className="pe-1">Identidad<span className="counseling-guion"> - </span></li>
-          <li className="pe-1">Adolescencia<span className="counseling-guion"> - </span></li>
-          <li className="pe-1">Pareja y familia<span className="counseling-guion"> - </span></li>
-          <li className="pe-1">Educación<span className="counseling-guion"> - </span></li>
-          <li className="pe-1">Adicciones<span className="counseling-guion"> - </span></li>
-          <li className="pe-1">Desarrollo personal<span className="counseling-guion"> - </span></li>
-          <li className="pe-1">Sexualidad<span className="counseling-guion"> - </span></li>
-          <li className="pe-1">Espiritualidad<span className="counseling-guion"> - </span></li>
+          <li className="pe-1">
+            Etapas vitales<span className="counseling-guion"> - </span>
+          </li>
+          <li className="pe-1">
+            Identidad<span className="counseling-guion"> - </span>
+          </li>
+          <li className="pe-1">
+            Adolescencia<span className="counseling-guion"> - </span>
+          </li>
+          <li className="pe-1">
+            Pareja y familia<span className="counseling-guion"> - </span>
+          </li>
+          <li className="pe-1">
+            Educación<span className="counseling-guion"> - </span>
+          </li>
+          <li className="pe-1">
+            Adicciones<span className="counseling-guion"> - </span>
+          </li>
+          <li className="pe-1">
+            Desarrollo personal<span className="counseling-guion"> - </span>
+          </li>
+          <li className="pe-1">
+            Sexualidad<span className="counseling-guion"> - </span>
+          </li>
+          <li className="pe-1">
+            Espiritualidad<span className="counseling-guion"> - </span>
+          </li>
           <li>Laboral</li>
         </ul>
       </section>
       <section className="pilares-cont">
         <h2>Pilares Fundamentales de nuestra profesión</h2>
         <div className="pilares-list">
-        <ul className="mb-4">
-          <li>
-            <span className="negritas">
-              Formación académica teórico-vivencial:
-            </span>{" "}
-            abordamos el potencial humano con sus aspectos biológicos,
-            psíquicos, sociológicos y espirituales. Con la responsabilidad de
-            una formación continua en todo lo que contribuya a nuestro
-            despliegue personal de modo creativo, a enriquecer nuestra
-            existencia como personas en el mundo, y a la facilitación de la
-            escucha como counselors.
-          </li>
-          <li>
-            <span className="negritas">Proceso personal</span>
-          </li>
-          <li>
-            <span className="negritas">Supervisión del trabajo</span>
-          </li>
-        </ul>
-        <p>
-          En estos pilares nos apoyamos para desarrollar las actitudes
-          personales y profesionales necesarias para crear condiciones
-          facilitadoras de un vínculo humano y una comunicación fluida en cada
-          encuentro.
-        </p>
+          <ul className="mb-4">
+            <li>
+              <span className="negritas">
+                Formación académica teórico-vivencial:
+              </span>{" "}
+              abordamos el potencial humano con sus aspectos biológicos,
+              psíquicos, sociológicos y espirituales. Con la responsabilidad de
+              una formación continua en todo lo que contribuya a nuestro
+              despliegue personal de modo creativo, a enriquecer nuestra
+              existencia como personas en el mundo, y a la facilitación de la
+              escucha como counselors.
+            </li>
+            <li>
+              <span className="negritas">Proceso personal:</span> en el que nos
+              acercamos a una comprensión de nosotros mismos y trabajamos
+              profundamente la autorrealización.
+            </li>
+            <li>
+              <span className="negritas">Supervisión del trabajo:</span>{" "}
+              mediante la cual revisamos e integramos nuestro ser personal,
+              nuestra formación y nuestra tarea profesional.
+            </li>
+          </ul>
+          <p>
+            En estos pilares nos apoyamos para desarrollar las actitudes
+            personales y profesionales necesarias para crear condiciones que
+            faciliten un vínculo humano en cada encuentro y el despliegue de la
+            persona a lo largo del proceso.
+          </p>
         </div>
       </section>
     </main>
